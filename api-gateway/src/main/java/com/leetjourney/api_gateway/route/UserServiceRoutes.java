6425ebb1bcb5b0fb7bf7f37c334dc247c9012ae1
risk-taker <deepuku5698@gmail.com>
2026-06-14 19:29:15 +0530

    commiting all

---
+++ api-gateway/src/main/java/com/leetjourney/api_gateway/route/UserServiceRoutes.java
@@ -0,0 +1,53 @@
+package com.leetjourney.api_gateway.route;
+
+import org.apache.catalina.Server;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class UserServiceRoutes {
+
+    @Bean
+    public RouterFunction<ServerResponse> userRoute() {
+        return route("user-service")
+                .route(RequestPredicates.path("/api/v1/user/**"), http())
+                .before(uri("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "userServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userFallbackRoute() {
+        return route("fallbackRoute")
+                .route(RequestPredicates.path("/fallbackRoute"),
+        request-> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("User service is down"))
+        .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceApiDocs() {
+        return GatewayRouterFunctions.route("user-service-api-docs")
+                .route(RequestPredicates.path("/docs/user-service/v3/api-docs"),
+                        http())
+                .before(uri("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+}
